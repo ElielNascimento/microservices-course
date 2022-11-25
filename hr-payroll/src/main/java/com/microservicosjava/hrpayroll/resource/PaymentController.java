fa2348fc0b8d6014ac3a78f47f96cd8fb4c17213
ElielNascimento <nascimentob.eliel@gmail.com>
2022-11-24 22:40:34 -0300

    entidade payment com dados statico

---
+++ hr-payroll/src/main/java/com/microservicosjava/hrpayroll/resource/PaymentController.java
@@ -0,0 +1,25 @@
+package com.microservicosjava.hrpayroll.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservicosjava.hrpayroll.entities.Payment;
+import com.microservicosjava.hrpayroll.service.PaymentService;
+
+@RestController
+@RequestMapping(value = "/api/payment")
+public class PaymentController {
+
+	@Autowired
+	private PaymentService paymentService;
+
+	@GetMapping("/{id}/days/{days}")
+	private ResponseEntity<Payment> getPayment(@PathVariable Long id, @PathVariable Integer days) {
+		Payment response = paymentService.PaymentCont(id, days);
+		return ResponseEntity.ok(response);
+	}
+}
---
+++ hr-payroll/src/main/java/com/microservicosjava/hrpayroll/service/PaymentService.java
@@ -0,0 +1,17 @@
+package com.microservicosjava.hrpayroll.service;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Service;
+
+import com.microservicosjava.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public Payment PaymentCont(long workerId, int days) {
+		return new Payment("Eliel", 250.00, days);
+
+	}
+}
