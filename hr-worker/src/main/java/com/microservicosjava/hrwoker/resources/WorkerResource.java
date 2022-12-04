47df2ade1360feea6672be2d054d5eeddfb946c2
ElielNascimento <nascimentob.eliel@gmail.com>
2022-12-04 13:10:01 -0300

    Configuring eureka server

--- hr-worker/src/main/java/com/microservicosjava/hrwoker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/microservicosjava/hrwoker/resources/WorkerResource.java
@@ -2,7 +2,6 @@ package com.microservicosjava.hrwoker.resources;
 
 import java.util.List;
 
- 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +19,11 @@ import com.microservicosjava.hrwoker.services.WorkerService;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
-	
-	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-	
+
 	@Autowired
 	private Environment env;
-	
-	
+
 	@Autowired
 	private WorkerService workerService;
 
@@ -40,8 +36,8 @@ public class WorkerResource {
 	@GetMapping("/{id}")
 	private ResponseEntity<Worker> workerById(@PathVariable Long id) {
 		logger.info("PORT =" + env.getProperty("local.server.port"));
-		
-		Worker response = workerService.workedById(id)	;
+
+		Worker response = workerService.workedById(id);
 		return ResponseEntity.ok(response);
 
 	}
