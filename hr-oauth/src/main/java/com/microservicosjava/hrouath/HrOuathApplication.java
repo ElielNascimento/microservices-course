bd5d70b6ee9e1d65cc55ba2c81a63aff444ba4dd
ElielNascimento <nascimentob.eliel@gmail.com>
2022-12-21 23:00:13 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/main/java/com/microservicosjava/hrouath/HrOuathApplication.java
@@ -0,0 +1,13 @@
+package com.microservicosjava.hrouath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOuathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOuathApplication.class, args);
+	}
+
+}
---
+++ hr-oauth/src/test/java/com/microservicosjava/hrouath/HrOuathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicosjava.hrouath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOuathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
